b0189519e7d2c82578af8e541bb37721c988d48c
Felipe118 <lipebezerraa@gmail.com>
2025-03-28 22:20:54 -0300

    feat: criando estrutura inicial migrations

---
+++ database/migrations/2025_03_28_005205_create_address_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('address', function (Blueprint $table) {
+            $table->id();
+            $table->string('address_name', 80)->unique();
+            $table->string('number', 10);
+            $table->string('complement', 50)->nullable();
+            $table->string('district', 50)->nullable();
+            $table->string('city', 50)->nullable();
+            $table->string('state', 40)->countries()->nullable();
+            $table->string('cep', 10);
+            $table->string('street');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('address');
+    }
+};
---
+++ database/migrations/2025_03_29_003058_create_restaurant_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('restaurant', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 80)->unique();
+            $table->string('perfil_img',100)->unique()->nullable();
+            $table->string('capa_img',0)->unique()->nullable();
+            $table->time('open_time')->nullable();
+            $table->time('close_time')->nullable();
+            $table->string('phone', 25)->unique();
+            $table->string('email',80)->unique()->nullable();
+            $table->boolean('active')->default(true);
+            $table->foreignId('address_id')->constrained('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('restaurant');
+    }
+};
